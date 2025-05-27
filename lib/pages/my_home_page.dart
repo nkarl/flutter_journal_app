8135561beaba66d853635087ed9c5ab80109b770
nkarl <21993921+nkarl@users.noreply.github.com>
2025-05-27 12:44:56 -0700

    Updated the styling of Home Page. Removed unused state field and added a snackbar for notifying on saved entry.

--- lib/pages/my_home_page.dart
+++ lib/pages/my_home_page.dart
@@ -12,20 +12,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  // defines an action to mark up the counter
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -74,7 +60,12 @@ class _MyHomePageState extends State<MyHomePage> {
             SizedBox(
               width: double.infinity,
               child: ElevatedButton(
-                onPressed: () {},
+                onPressed: () {
+                  // Placeholder: Show a snackbar to indicate saving (replace with actual save logic)
+                  ScaffoldMessenger.of(
+                    context,
+                  ).showSnackBar(const SnackBar(content: Text('Entry saved!')));
+                },
                 child: const Text('Save Entry'),
               ),
             ),
@@ -92,11 +83,6 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
