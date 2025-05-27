a18adb85e5bc1d72e741660b150df8b034e03195
nkarl <21993921+nkarl@users.noreply.github.com>
2025-05-27 12:27:33 -0700

    Added the styling for the Home Page, which let the user immediately write a journal entry upon opening the app.

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyJournalApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Journal App',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -29,9 +29,9 @@ class MyJournalApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(), // title: 'Flutter Journal App Home Page'),
     );
   }
 }
--- lib/pages/my_home_page.dart
+++ lib/pages/my_home_page.dart
@@ -2,16 +2,8 @@
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  // constructor
+  const MyHomePage({super.key, this.title = "New Journal Entry"});
 
   final String title;
 
@@ -22,6 +14,7 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
+  // defines an action to mark up the counter
   void _incrementCounter() {
     setState(() {
       // This call to setState tells the Flutter framework that something has
@@ -43,37 +36,58 @@ class _MyHomePageState extends State<MyHomePage> {
     // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Padding(
+        // Padding is a layout widget. It takes a column of widgets as child
+        // and add padding around it.
+        padding: const EdgeInsets.all(16.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            // Text field for Entry Title
+            TextField(
+              decoration: const InputDecoration(
+                labelText: 'Title',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 16.0),
+
+            // Expanded Text Field for Entry Content
+            Expanded(
+              child: TextField(
+                decoration: const InputDecoration(
+                  labelText: "Content",
+                  border: OutlineInputBorder(),
+                  alignLabelWithHint: true,
+                ),
+                maxLines: null,
+                expands: true,
+                textAlignVertical: TextAlignVertical.top,
+              ),
+            ),
+            const SizedBox(height: 16.0),
+
+            // Button to save Entry
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: const Text('Save Entry'),
+              ),
+            ),
+            const SizedBox(height: 16.0),
+
+            // a row of buttons for navigating to the journal list, login and settings
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                TextButton(onPressed: () {}, child: const Text("Journal List")),
+                TextButton(onPressed: () {}, child: const Text("Login")),
+                TextButton(onPressed: () {}, child: const Text("Settings")),
+              ],
             ),
           ],
         ),
