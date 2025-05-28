d5eb7d8f1bb88b2bb99d036ad61a3913421c6125
nkarl <21993921+nkarl@users.noreply.github.com>
2025-05-28 13:56:57 -0700

    Added a debug page to test cloud Firebase connection.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:journal_app/pages/debug_page.dart';
 import 'pages/entry_page.dart';
 
 void main() {
@@ -32,6 +33,7 @@ class MyJournalApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
       ),
       home: const EntryPage(), // title: 'Flutter Journal App Home Page'),
+      //home: const DebugPage(),
     );
   }
 }
