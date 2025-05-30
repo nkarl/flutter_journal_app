00b8e79a1f0d150889be550a7ecc8dc71564bca1
nkarl <21993921+nkarl@users.noreply.github.com>
2025-05-29 18:19:47 -0700

    Added a Stateless Login Page.

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,45 @@
+// File: lib/screens/login_page.dart
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Login')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            const TextField(
+              decoration: InputDecoration(
+                labelText: 'Email',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            const TextField(
+              decoration: InputDecoration(
+                labelText: 'Password',
+                border: OutlineInputBorder(),
+              ),
+              obscureText: true,
+            ),
+            const SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: () {
+                if (kDebugMode) {
+                  print('Login button pressed');
+                }
+                Navigator.of(context).pop();
+              },
+              child: const Text('Login'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
